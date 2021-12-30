4dd0ba10d6f4e253b3cc37d70d03e9616c040177
jimmym620 <jimmym620@gmail.com>
2021-12-30 12:35:26 +0000

    initial design of page

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <header>
+                <h1>Chun Ming Jimmy Man</h1>
+            </header>
+            <main>
+                <div className="navbar"></div>
+                <article className="current-page"></article>
+                <div className="socialBtns"></div>
+            </main>
+        </>
+    );
 }
 
 export default App;
