df925a12548d2962b6ef229eb88a2be68926d808
jimmym620 <jimmym620@gmail.com>
2021-12-30 13:26:05 +0000

    basic react router setup

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,5 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
     return (
@@ -8,11 +9,70 @@ function App() {
                 <h1>Chun Ming Jimmy Man</h1>
             </header>
             <main>
-                <div className="navbar"></div>
+                <Router>
+                    <div>
+                        <nav>
+                            <ul>
+                                <li>
+                                    <Link to="/">Home</Link>
+                                </li>
+                                <li>
+                                    <Link to="/about">About</Link>
+                                </li>
+                                <li>
+                                    <Link to="/pages">Pages</Link>
+                                </li>
+                                <li>
+                                    <Link to="/contact">Contact</Link>
+                                </li>
+                            </ul>
+                        </nav>
+                        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+                        <Routes>
+                            <Route path="/" element={<Home />} />
+                            <Route path="/about" element={<About />} />
+                            <Route path="/pages" element={<Pages />} />
+                            <Route path="/contact" element={<Contact />} />
+                        </Routes>
+                    </div>
+                </Router>
+
                 <article className="current-page"></article>
                 <div className="socialBtns"></div>
             </main>
         </>
+    );
+}
+
+function Home() {
+    return (
+        <div>
+            <h2>Home</h2>
+        </div>
+    );
+}
+
+function About() {
+    return (
+        <div>
+            <h2>About</h2>
+        </div>
+    );
+}
+
+function Pages() {
+    return (
+        <div>
+            <h2>My Pages</h2>
+        </div>
+    );
+}
+function Contact() {
+    return (
+        <div>
+            <h2>Contact me</h2>
+        </div>
     );
 }
 
