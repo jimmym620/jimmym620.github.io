cfc5a3efa361eda409288d682806ffabf80c43cb
jimmym620 <jimmym620@gmail.com>
2022-01-01 15:47:36 +0000

    updated weather site link

--- portfolio/src/pages-projects-data.js
+++ portfolio/src/pages-projects-data.js
@@ -3,15 +3,15 @@ export default [
     {
         id: 1,
         title: "Weather app",
-        link: "https://jimmym620.github.io/weather_app/",
+        link: "https://jimmym620-weather-app.netlify.app",
         tag: "React",
         desc: "This project was made using the OpenWeatherMap API which gave access to weather statistics for the current day and for the coming week, I also used their geocode API to get the longitude and latitude of the input cities.",
     },
     {
         id: 2,
         title: "Project 2",
-        link: "https://jimmym620.github.io",
+        link: "none",
         tag: "None",
-        desc: "Next project"
+        desc: "Next project",
     },
 ];
